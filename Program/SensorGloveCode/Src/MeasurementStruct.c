2a70ad89a95ef87b4d641b27fc9cbf54cc66c8d1
Krzysztof Dąbek <k.narg.dabek@gmail.com>
2017-04-02 23:55:23 +0200

    Ostatnie poprawki do Bluetooth, Przesylanie dziala

--- Program/SensorGloveCode/Src/MeasurementStruct.c
+++ Program/SensorGloveCode/Src/MeasurementStruct.c
@@ -12,32 +12,36 @@ HAL_StatusTypeDef TransmitMeasurementsBluetooth(s_measurements p_Measurements)
 	uint8_t i;
 	uint8_t OutputData[2];
 	uint16_t MessageSize;
+	HAL_StatusTypeDef TransmisionStatus;
+	HAL_GPIO_WritePin(LD9_GPIO_Port,LD9_Pin,GPIO_PIN_SET);
+
+	//Sending Flex sensor measurements
 	for(i=0;i<FLEX_SENSOR_COUNT;++i)
 	{
 		MessageSize=sprintf(OutputData,"%d ",p_Measurements.FlexSensor[i]);
-		if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-			return HAL_ERROR;
+		TransmisionStatus=HAL_UART_Transmit(&huart4,OutputData,MessageSize,TRANSMISION_TIMEOUT);
+		if(TransmisionStatus!=HAL_OK)
+			return TransmisionStatus;
 	}
-	MessageSize=sprintf(OutputData,"\n\r");
-	if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-		return HAL_ERROR;
+
+	//Sending Tension sensor measurements
 	for(i=0;i<TENSION_SENSOR_COUNT;++i)
 	{
 		MessageSize=sprintf(OutputData,"%d ",p_Measurements.TensionSensor[i]);
-		if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-			return HAL_ERROR;
+		TransmisionStatus=HAL_UART_Transmit(&huart4,OutputData,MessageSize,TRANSMISION_TIMEOUT);
+		if(TransmisionStatus!=HAL_OK)
+			return TransmisionStatus;
 	}
-	MessageSize=sprintf(OutputData,"\n\r");
-	if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-		return HAL_ERROR;
+
+	//Sending Accelerometer measurements
 	for(i=0;i<ACCELEROMETER_AXIS_COUNT;++i)
 	{
 		MessageSize=sprintf(OutputData,"%d ",p_Measurements.Accelerometer[i]);
-		if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-			return HAL_ERROR;
+		TransmisionStatus=HAL_UART_Transmit(&huart4,OutputData,MessageSize,TRANSMISION_TIMEOUT);
+		if(TransmisionStatus!=HAL_OK)
+			return TransmisionStatus;
 	}
-	MessageSize=sprintf(OutputData,"\n\r");
-	if(HAL_UART_Transmit_IT(&huart4,OutputData,MessageSize)==HAL_ERROR)
-		return HAL_ERROR;
+
+	HAL_GPIO_WritePin(LD9_GPIO_Port,LD9_Pin,GPIO_PIN_RESET);
 	return HAL_OK;
 }
